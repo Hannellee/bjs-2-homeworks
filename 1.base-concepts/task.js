e9c9e62b3f524f3ba734fe6e386be2bc00650955
Anna Bolgova <97449480+Hannellee@users.noreply.github.com>
2022-04-19 19:50:57 +0300

    Add files via upload
    first commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,64 @@
+'use strict';
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  let d = b**2 - 4*a*c;
+
+  if (d < 0) {
+    return arr;
+  }
+
+  else if (d === 0) {
+    let res = -b/(2*a);
+    arr.push(res);
+    return arr;
+  }
+
+  else {
+    arr.push(((-b + Math.sqrt(d) )/(2*a)));
+    arr.push(((-b - Math.sqrt(d) )/(2*a)));
+    return arr;
+  }
 }
+
+'use strict';
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
-  // код для задачи №2 писать здесь
+  if (isNaN(percent)) {
+    console.log(`Параметр "Процентная ставка" содержит неправильное значение ${percent}`);
+    totalAmount = `Параметр "Процентная ставка" содержит неправильное значение ${percent}`;
+    return totalAmount;
+  } else if (isNaN(contribution)) {
+    console.log(`Параметр "Начальный взнос" содержит неправильное значение ${contribution}`);
+    totalAmount = `Параметр "Начальный взнос" содержит неправильное значение ${contribution}`;
+    return totalAmount;
+  } else if (isNaN(amount)) {
+    console.log(`Параметр "Общая стоимость" содержит неправильное значение ${amount}`);
+    totalAmount = `Параметр "Общая стоимость" содержит неправильное значение ${amount}`;
+    return totalAmount;
+  } else {
+    percent = Number(percent);
+    contribution = Number(contribution);
+    amount = Number(amount);
 
-  return totalAmount;
+
+    let creditBody = amount - contribution;
+
+    let dateNow = Number(new Date());
+    let dateInput = date.getTime();
+
+    let monthsQuantity = (dateInput - dateNow) / (1000 * 60 * 60 * 24 * 30.42);
+    monthsQuantity = Math.round(monthsQuantity);
+
+    percent = percent / 1200;
+
+    let amountPerMonth = creditBody * (percent + (percent / (((1 + percent) ** monthsQuantity) - 1)))
+
+    totalAmount = Number((amountPerMonth * monthsQuantity).toFixed(2));
+
+    console.log(`Обая сумма кредита равна ${totalAmount}`)
+    return totalAmount;
+  }
 }
