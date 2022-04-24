cb161db2dd3f5d4cf46f5040e37d3942cdb7d6c9
Anna Bolgova <97449480+Hannellee@users.noreply.github.com>
2022-04-24 16:48:29 +0300

    Add files via upload
    first commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,18 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  result = arr1.every(function(item, idx, arr1) {
+    return item === arr2[idx] && arr1.length === arr2.length;
+  })
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
+  resultArr = arr.filter((item) => item > 0);
+  resultArr = resultArr.filter((item) => item % 3 === 0);
+  resultArr = resultArr.map((item) => item * 10);  
 
   return resultArr; // array
 }
