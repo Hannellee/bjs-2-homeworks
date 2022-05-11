b73b34dd5dafea587c6dbc298b648fb02bb322dd
Anna Bolgova <97449480+Hannellee@users.noreply.github.com>
2022-05-11 17:29:13 +0300

    Add files via upload
    first commit
    help needed

---
+++ 6.exception-closure/task.js
@@ -0,0 +1,62 @@
+// Задача 1
+
+
+function parseCount(value) {
+    let number = Number.parseInt(value, 10);
+    if(isNaN(number)) {
+        throw new Error('Невалидное значение');
+    } else {
+        return number;
+    }
+}
+
+function validateCount(value) {
+    try {
+        return parseCount(value);
+    }
+    catch(err) {
+        return err;
+    }
+}
+
+
+// Задача 2
+
+
+class Triangle {
+    constructor(a, b, c) {
+        if(((a + b) < c) || ((b + c) < a) || ((a + c) < b)) {
+            throw new Error('Треугольник с такими сторонами не существует');
+
+            this.a = a;
+            this.b = b;
+            this.c = c;            
+        }
+    }
+
+    getPerimeter() {
+        return Number(this.a + this.b + this.c);
+        
+    }
+
+    getArea() {
+        let p = this.getPerimeter();
+        p = p / 2;
+
+        let s = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+        return Number(s.toFixed(3));
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } 
+    catch(err) {
+        let errorCatcer = {};
+        errorCatcer.getArea = function() {return ('Ошибка! Треугольник не существует')};
+        errorCatcer.getPerimeter = function() {return ('Ошибка! Треугольник не существует')};
+        
+        return errorCatcer;
+    }
+}
