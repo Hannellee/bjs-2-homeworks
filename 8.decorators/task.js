299422e5ab0a77ed63f4a605c2071878b1c1761d
Anna Bolgova <97449480+Hannellee@users.noreply.github.com>
2022-06-04 16:58:22 +0300

    Add files via upload
    first commit

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,53 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+function wrapper(...args) {
+    const hash = args.join(',');
+    let idx = cache.findIndex((item) => item.hash == hash);
+    if (idx !== -1) {
+      console.log("Из кэша: " + cache[idx].result);
+      return "Из кэша: " + cache[idx].result;
+    }
+
+    let result = func(...args); // в кэше результата нет - придётся считать
+    cache.push({hash, result}) ; // добавляем элемент с правильной структурой
+    if (cache.length > 5) { 
+      cache.shift(); // если слишком много элементов в кэше надо удалить самый старый (первый) 
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;  
+}
+return wrapper;
 }
 
 
 function debounceDecoratorNew(func) {
-  // Ваш код
+  let timeout;  
+    function wrapper(...args) {
+      if (timeout === null) {
+        func(...args);
+      }
+
+      clearTimeout(timeout);
+      timeout = setTimeout(() => func(...args), ms);
+    }
+
+    return wrapper;
 }
 
 function debounceDecorator2(func) {
-  // Ваш код
+  let timeout = null;
+
+    function wrapper(...args) {
+      if (timeout === null) {
+        func(...args);
+      }
+      
+      clearTimeout(timeout);
+      timeout = setTimeout(() => func(...args), ms);
+      wrapper.count++;
+    }
+
+    wrapper.count = 0;
+    return wrapper;
 }
